7b4cbefa312529595c0c50f2a5ebd39c747af708
JF Padilla <emailreel@gmail.com>
2018-01-12 23:35:37 +0000

    update ad index view, update ad.ctrlr new action and its specs

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -7,6 +7,7 @@ class AdvertisementsController < ApplicationController
   end
 
   def new
+    @advertisement = Advertisement.new
   end
 
   def create
--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -29,6 +29,16 @@ RSpec.describe AdvertisementsController, type: :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
+    
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "instantiates @advertisement" do
+      get :new
+      expect(assigns(:advertisement)).not_to be_nil
+    end    
   end
 
   describe "GET #create" do
