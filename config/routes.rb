9202983970d99c7fe1c2acb54fec2bf41d83d781
JF Padilla <emailreel@gmail.com>
2018-01-12 22:47:12 +0000

    add advertisement controller and its default specs, modify routes to employ resources syntax and have default adv.ctrlr. specs pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "advertisements/show" # required to pass default adv.ctrl. specs pass
+  
+  resources :advertisements
+
   resources :posts
   
   get 'about' => 'welcome#about'
