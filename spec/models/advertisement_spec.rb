b9e748c5f8f4c714780a3ed574b16e591bf80d08
JF Padilla <emailreel@gmail.com>
2018-01-12 21:03:02 +0000

    add advertisement model and its spec

---
+++ spec/models/advertisement_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Advertisement, type: :model do
+    let(:advertisement) { Advertisement.create!(
+        title: "New Advertisement Title", copy: "New Advertisement Copy",
+        price: 10) }
+  
+    describe "attributes" do
+        it "has title and body attributes" do
+            expect(advertisement).to have_attributes(
+                title: "New Advertisement Title", 
+                copy: "New Advertisement Copy",
+                price: 10)
+        end
+    end
+end
