0421f3b77da879e64629a215d66f866f88805667
JF Padilla <emailreel@gmail.com>
2018-01-13 12:47:52 +0000

    update seed generator to generate random ads, update ad index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,18 @@
    )
  end
  
+  # Create Ads
+ 50.times do
+   Advertisement.create!(
+     title:  RandomData.random_sentence,
+     copy:   RandomData.random_paragraph,
+     price:  rand(1..1000)
+   )
+ end
+ posts = Advertisement.all
+ 
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "#{Advertisement.count} ads created"
  
