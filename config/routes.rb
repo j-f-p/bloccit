a9e47b2fda723608ded21b5b254360a715f9e0bc
JF Padilla <emailreel@gmail.com>
2018-01-15 20:27:04 +0000

    remove excess routes leftover from beginning of project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'questions/show' # required to pass default questions ctrllr specs
-  get 'questions/edit' # required to pass default questions ctrllr specs
-
   resources :questions
 
   resources :posts
