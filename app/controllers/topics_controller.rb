eb92108210814d2c1f173982dd2431150a6f2553
JF Padilla <emailreel@gmail.com>
2018-01-18 16:34:28 +0000

    add Topics create specs and action

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -10,4 +10,18 @@ class TopicsController < ApplicationController
   def new
     @topic = Topic.new
   end
+  
+  def create
+    @topic = Topic.new
+    @topic.name = params[:topic][:name]
+    @topic.description = params[:topic][:description]
+    @topic.public = params[:topic][:public]
+
+    if @topic.save
+      redirect_to @topic, notice: "Topic was saved successfully."
+    else
+      flash.now[:alert] = "Error creating topic. Please try again."
+      render :new
+    end
+  end
 end
--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -48,5 +48,25 @@ RSpec.describe TopicsController, type: :controller do
       get :new
       expect(assigns(:topic)).not_to be_nil
     end
-end
+  end
+
+  describe "POST create" do
+    it "increases the number of topics by 1" do
+      expect{ post :create, params: {topic: { name: RandomData.random_sentence,
+                                      description: RandomData.random_paragraph }
+                            }}.to change(Topic,:count).by(1)
+    end
+  
+    it "assigns Topic.last to @topic" do
+      post :create, params: {topic: { name: RandomData.random_sentence, 
+                              description: RandomData.random_paragraph }}
+      expect(assigns(:topic)).to eq Topic.last
+    end
+
+    it "redirects to the new topic" do
+      post :create, params: {topic: { name: RandomData.random_sentence, 
+                              description: RandomData.random_paragraph }}
+      expect(response).to redirect_to Topic.last
+    end
+  end
 end
