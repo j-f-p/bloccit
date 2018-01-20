10b57dcc32f495ea36c06e7b617aa84c5d7d2b64
JF Padilla <emailreel@gmail.com>
2018-01-20 12:42:44 +0000

    update routes and topics show view to account for sponsors
    update routes by replacing sponsors root routes by resources routes
    nested under topics resources routes,
    update topics show view to display sponsors below posts
    confirm that sponsor model spec continues to pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'sponsors/show'
-
-  get 'sponsors/new'
-
-  get 'sponsors/edit'
-
   resources :topics do
     resources :posts, except: [:index]
+    resources :sponsors, except: [:index]
   end
   
   get 'about' => 'welcome#about'
