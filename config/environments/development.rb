5bfc229556c731b27e4f0d8b5a5756ad50232e1b
JF Padilla <emailreel@gmail.com>
2018-02-10 00:27:20 +0000

    install SendGrid and setup action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { host:
+  'https://976910ca881740cfa3cf8b7964a3c9f5.vfs.cloud9.us-east-2.amazonaws.com'}
 end
