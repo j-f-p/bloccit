5f4f0967de8d4636cb7352eba2048286c4a74d3f
JF Padilla <emailreel@gmail.com>
2018-02-14 17:41:14 +0000

    add users view helper methods about posts and comments
    define helper method about rendering posts
    define helper method about rendering comments
    update users show view to include these methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
 module UsersHelper
+  def user_posts
+    if @user.posts.count > 0
+      concat content_tag(:h2, "Posts")
+      concat render @user.posts
+    else
+      concat content_tag(:p, "#{@user.name} has not submitted any posts yet.")
+    end
+  end
+  
+  def user_comments
+    if @user.comments.count > 0
+      concat content_tag(:h2, "Comments")
+      concat render @user.comments
+    else
+      concat content_tag(:p,
+        "#{@user.name} has not submitted any comments yet.")
+    end
+  end
 end
